eaf99b53309c389942d29f9fe4c18332f7d4a88c
Victor Punko <workvictor@yandex.ru>
2017-06-02 22:31:38 +0300

    added templates generating

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 window.onload = main();
 
-function main(){
-    var player = new Player({
-        sourceID: 'cPlay__audioSource',
-        title:'cPlay__title'
-    });    
-       
+function main() {
+  var player = new Player({
+    //options
+    mountPoint: "cPlayMountPoint",
+    coverImg: "cover.jpg",
+    // title: false,
+    titleText: "Scandroid-Connection",
+    // cover: false,
+  });
+
 }
--- player/custom_player.js
+++ player/custom_player.js
@@ -1,222 +1,367 @@
 function Player(options) {
-     // cSpell:ignoreRegExp /[а-я]/i
-     //определяем настройки по умолчанию
-     this.scrDir = './player/';
-     optionsDefault = {
-          //поиск по ID
-          sourceID: 'cPlay__audioSource',
-          //исключение поиск по классу
-          controlBtnClass: 'cPlay__btn',
-          title: 'cPlay__title',
-          progressBar: 'cPlay__bar',
-          progressBarInner: 'cPlay__bar_inner',
-          progressBarBuffer: 'cPlay__bar_buffer',
-          timeNow: 'cPlay__timeNow',
-          timeTotal: 'cPlay__timeTotal',
-          volume: 'cPlay__volume',
-          volumePicker: 'cPlay__volumePicker',
-          buffering: true,
-          snapToBuffer: true,
-     }
+  // cSpell:ignoreRegExp /[а-я]/i
+  //определяем настройки по умолчанию
 
-     this.options = optionsDefault;
-     //если добавлены опции - проверяем их и перезаписываем опции по умолчанию
-     if (options !== undefined) {
-          this.getProps(options);
-     }
+  var mountOptions = {
+    //required
+    mountPoint: "cPlayMountPoint",
+    main: "cPlay",
+    source: "./player/Scandroid-Connection.mp3",
+    content: "cPlayContent",
+    playBtn: "cPlayControls__btn_play",
+    pauseBtn: "cPlayControls__btn_pause",
 
-     this.source = this.getElem(this.options.sourceID);
-     this.player_title = this.getElem(this.options.title);
+    //optional    
+    cover: "cPlayCover",
+    coverImg: "cover.jpg",
+    title: "cPlayContent__title",
+    titleInner: "cPlayTitleInner",
+    titleText: "cPlay title",
+    progressBox: "cPlayContentProgress",
+    bar: "cPlayProgressBar",
+    buffer: "cPlayProgressBar__buffer",
+    progress: "cPlayProgressBar__inner",
+    time: "cPlayContentProgress__time",
+    timeNow: "cPlayContentProgress__time_now",
+    timeTotal: "cPlayContentProgress__time_total",
+    controls: "cPlayControls",
+    controlBtn: "cPlayControls__btn",
+    controlBtnActive: "cPlayControls__btn_active",
+    stopBtn: "cPlayControls__btn_stop",
+    volume: 'cPlayVolume',
+    volumePicker: 'cPlayVolume__picker',
+    buffering: true,
+    snapToBuffer: true,
+  }
 
-     this.controlButtons = document.getElementsByClassName("custom-player__btn");
-     this.playBtn = document.getElementById("player_play"); //создать программно по атрибуту role
-     this.stopBtn = document.getElementById("player_stop"); //создать программно по атрибуту role
-     this.title_inner = document.getElementById("title_inner"); //создать программно
-     this.player_progress = document.getElementById("player_progress");
-     this.progress_buffer = document.getElementById("progress_buffer");
-     this.progress_inner = document.getElementById("progress_inner");
-     this.player_timeTotal = document.getElementById("player_timeTotal");
-     this.player_timeNow = document.getElementById("player_timeNow");
-     this.totalCalculated = false;
-     this.snapToBufferLoaded = true;
+  this.mountOptions = mountOptions;
+  //если добавлены опции - проверяем их и перезаписываем опции по умолчанию
+  if (options !== undefined) {
+    this.resetOptions(options);
+  }
 
-     this.init();
+  this.generateTemplate();
 
-     //TODO: add snapping option
-     //TODO: add volume option
+  this.init();
+
+  //TODO: add snapping option
+  //TODO: add volume option
 }
 
-Player.prototype.getElem = function(elemName) {
-     var elem = null;
-     //поиск по ID
-     try {
-          elem = document.getElementById(elemName);
-          if (elem) {
-               if (elem.hasAttribute('role')) {
-                    elem.role = elem.getAttribute('role');
-               }
-          } else {
-               throw ("Обращение к несуществующему элементу. Нет такого элемента - <b>" + elemName + "</b>");
-          }
-     } catch (error) {
-          var output = error;
-          this.generateMsg(output);
-     };
-     return elem;
-}
+Player.prototype.generateTemplate = function() {
+  var mountOptions = this.mountOptions;
 
-Player.prototype.generateMsg = function(msg) {
-     var elem = document.createElement('div');
-     var styles = document.createElement('link');
-     styles.rel = "stylesheet";
-     styles.href = this.scrDir + 'cPlay__error.css';
-     elem.innerHTML = msg;
-     document.body.appendChild(elem);
-     document.head.appendChild(styles);
-     if (elem.classList) {
-          elem.classList.add('cPlay__error');
-          setTimeout(function() {
-               elem.classList.add('cPlay__error_active');
-          }, 100)
-     }
-}
+  //undefined variables
+  var
+    cPlay, mount, mountPoint, content,
+    cover, title, title_inner, bar, buffer,
+    progress, timeNow, timeTotal, controls,
+    playBtn, pauseBtn, stopBtn, volume,
+    volumePicker, staticPath;
 
-Player.prototype.getProps = function(options) {
-     for (var property in options) {
-          if (this.options.hasOwnProperty(property)) {
-               this.options[property] = options[property];
-          }
-          if (!this.options.hasOwnProperty(property)) {
-               console.warn('Нет такой опции:', property, 'Доступные опции: ', this.options);
-               delete options[property];
-          }
-     }
-     return options;
-}
+  mount = document.getElementById(mountOptions.mountPoint);
+  if (!mount) mount = document.getElementsByClassName(mountOptions.mountPoint)[0];
+  if (mount.hasAttribute('src')) {
+    mount.src = mount.getAttribute('src');
+    staticPath = getStaticPath(mount.src);
 
-Player.prototype.update = function() {
-     if (this.playBack !== false) {
-          this.updateTime(this.player_timeTotal, this.source.duration);
-          this.updateTime(this.player_timeNow, this.source.currentTime);
-          this.updateProgress();
-          setTimeout(this.update.bind(this), 1000);
-     }
-}
+    cPlay = mountNode(mount, mountOptions.main);
+    mountPoint = cPlay;
+    cover = (mountOptions.cover) ? addCover(mountPoint) : undefined;
 
-Player.prototype.setBuffering = function(parameter) {
-     this.buffering = parameter;
-}
+    content = mountNode(mountPoint, mountOptions.content);
+    mountPoint = content;
 
-Player.prototype.updateProgress = function() {
-     if (this.source.duration) {
-          this.progress = Math.floor((this.source.currentTime / this.source.duration) * 100);
-          this.progress_inner.style.width = this.progress + '%';
-     } else {
-          this.progress_inner.style.width = 0 + '%';
-     }
-     this.playBack = (this.progress == 100) ? false : this.playBack;
+    if (mountOptions.title) {
+      title = mountNode(mountPoint, mountOptions.title);
+      title_inner = title.appendChild(addElem(mountOptions.titleInner))
+      title_inner.innerHTML = mountOptions.titleText;
+    }
+
+    bar = mountNode(mountPoint, mountOptions.bar);
+    buffer = bar.appendChild(addElem(mountOptions.buffer));
+    progress = bar.appendChild(addElem(mountOptions.progress));
+
+    timeNow = addClassName(mountPoint.appendChild(addElem(mountOptions.time)), mountOptions.timeNow);
+    timeTotal = addClassName(mountPoint.appendChild(addElem(mountOptions.time)), mountOptions.timeTotal);
+    timeNow.innerHTML = '00:00';
+    timeTotal.innerHTML = '00:00';
+
+    controls = mountNode(mountPoint, mountOptions.controls);
+    playBtn = addClassName(controls.appendChild(addElem(mountOptions.controlBtn)), mountOptions.playBtn);
+    pauseBtn = addClassName(controls.appendChild(addElem(mountOptions.controlBtn)), mountOptions.pauseBtn);
+    stopBtn = (mountOptions.stopBtn !== false) ? addClassName(controls.appendChild(addElem(mountOptions.controlBtn)), mountOptions.stopBtn) : undefined;
+
+    volume = (mountOptions.volume) ? mountNode(controls, mountOptions.volume) : undefined;
+    volumePicker = (volume) ? volume.appendChild(addElem(mountOptions.volumePicker)) : undefined;
+  } else {
+    console.error('error: source not found');
+    console.log('object: ', mount);
+    console.log('"src" attribute is required!');
+  }
+
+  this.src = mount.src;
+  this.title = title;
+  this.title_inner = title_inner;
+  this.progress_bar = bar;
+  this.progress_buffer = buffer;
+  this.progress_current = progress;
+  this.player_timeTotal = timeTotal;
+  this.player_timeNow = timeNow;
+  this.playBtn = playBtn;
+  this.pauseBtn = pauseBtn;
+  this.stopBtn = stopBtn;
+  this.controlButtons = document.getElementsByClassName(mountOptions.controlBtn);
+  this.volume_bar = volume;
+  this.volume_pick = volumePicker;
+
+
+
+  function addElem(mountClass) {
+    var elem = document.createElement('div');
+    if (elem.classList) {
+      elem.classList.add(mountClass);
+    }
+    return elem;
+  }
+
+  function addClassName(obj, className) {
+    if (obj.classList) {
+      obj.classList.add(className);
+    }
+    return obj;
+  }
+
+  function addCover(mountPoint) {
+    var cover = mountPoint.appendChild(addElem(mountOptions.cover));
+    var coverImg = document.createElement('img');
+    cover.appendChild(coverImg);
+    coverImg.src = staticPath + mountOptions.coverImg;
+    return cover;
+  }
+
+  function mountNode(mountPoint, mountElem) {
+    return mountPoint.appendChild(addElem(mountElem));
+  }
+
+  function getStaticPath(src) {
+    var del = /([\w\.\ \-\_]+)\.\w+/;
+    return src.replace(del, '');
+  }
 }
 
 Player.prototype.init = function() {
-     this.setBuffering(true);
-     this.player_timeTotal.innerHTML = "00:00";
-     this.progress = 0;
-     this.playBack = false;
-     this.initControls();
-     if (this.buffering) {
-          this.source.preload = "auto";
-          this.updateBuffer();
-     } else {
-          this.source.preload = "none";
-     }
-     this.updateProgress();
-}
-Player.prototype.updateBuffer = function() {
-     var bufferProgress = this.calculateBuffer();
-     if (bufferProgress < 100 || bufferProgress == undefined) {
-          setTimeout(this.updateBuffer.bind(this), 1000);
-     }
+  if (this.src) {
+    this.source = new Audio();
+    this.source.volume = 0.5;
 
-     this.updateTime(this.player_timeTotal, this.source.duration);
-     this.updateTime(this.player_timeNow, this.source.currentTime);
+    this.source.src = this.src;
+    this.setBuffering(true);
+    this.player_timeTotal.innerHTML = "00:00";
+    this.progress = 0;
+    this.playBack = false;
+    this.initControls();
+    if (this.buffering) {
+      this.source.preload = "auto";
+      this.updateBuffer();
+    } else {
+      this.source.preload = "none";
+    }
+    this.updateProgress();
+  }
+
+}
+Player.prototype.resetOptions = function(options) {
+  var setOptions = this.mountOptions;
+  for (var property in options) {
+    if (this.mountOptions.hasOwnProperty(property)) {
+      setOptions[property] = options[property];
+    }
+  }
+  return setOptions;
+}
+Player.prototype.getProps = function(options) {
+  var opt = this.mountOptions;
+  for (var property in options) {
+    if (this.options.hasOwnProperty(property)) {
+      this.options[property] = options[property];
+    }
+    if (!this.options.hasOwnProperty(property)) {
+      console.warn('Нет такой опции:', property, 'Доступные опции: ', this.options);
+      delete options[property];
+    }
+  }
+  return options;
+}
+
+Player.prototype.update = function() {
+  if (this.playBack !== false) {
+    this.updateTime(this.player_timeTotal, this.source.duration);
+    this.updateTime(this.player_timeNow, this.source.currentTime);
+    this.updateProgress();
+    setTimeout(this.update.bind(this), 1000);
+  }
+}
+
+Player.prototype.setBuffering = function(parameter) {
+  this.buffering = parameter;
+}
+
+Player.prototype.updateProgress = function() {
+  if (this.source.duration) {
+    this.progress = Math.floor((this.source.currentTime / this.source.duration) * 100);
+    this.progress_current.style.width = this.progress + '%';
+  } else {
+    this.progress_current.style.width = 0 + '%';
+  }
+  this.playBack = (this.progress == 100) ? false : this.playBack;
+}
+
+Player.prototype.updateVolume = function() {
+  this.source.volume = 0.5;
+}
+
+Player.prototype.updateBuffer = function() {
+  var bufferProgress = this.calculateBuffer();
+  if (bufferProgress < 100 || bufferProgress == undefined) {
+    setTimeout(this.updateBuffer.bind(this), 1000);
+  }
+
+  this.updateTime(this.player_timeTotal, this.source.duration);
+  this.updateTime(this.player_timeNow, this.source.currentTime);
 }
 
 Player.prototype.calculateBuffer = function() {
-     this.progress_buffer.style.width = 0 + '%';
-     if (this.source.buffered.length != 0) {
-          var buffer = this.source.buffered.end(this.source.buffered.length - 1) - this.source.buffered.start(0);
-          var bufferProgress = Math.floor((buffer / this.source.duration) * 100);
-          this.progress_buffer.style.width = bufferProgress + '%';
-     }
-     return bufferProgress;
-}
-Player.prototype.mouseMove = function(event) {
-     var widthTotal = getComputedStyle(this.player_progress).width.slice(0, -2);
-     var rect = event.target.getBoundingClientRect();
-     var x = event.clientX - rect.left;
-     this.selectedProgress = Math.floor((x / widthTotal) * 100);
-     if (this.selectedProgress > this.calculateBuffer()) {
-          this.selectedProgress = this.calculateBuffer();
-     }
-     this.source.currentTime = this.source.duration * (this.selectedProgress / 100);
-     this.updateTime(this.player_timeNow, this.source.currentTime);
-     this.updateProgress();
+  this.progress_buffer.style.width = 0 + '%';
+  if (this.source.buffered.length != 0) {
+    var buffer = this.source.buffered.end(this.source.buffered.length - 1) - this.source.buffered.start(0);
+    var bufferProgress = Math.floor((buffer / this.source.duration) * 100);
+    this.progress_buffer.style.width = bufferProgress + '%';
+  }
+  return bufferProgress;
 }
 
 Player.prototype.clearActiveState = function() {
-     for (var i = 0; i < this.controlButtons.length; i++) {
-          if (this.controlButtons[i].classList !== undefined)
-               this.controlButtons[i].classList.remove("custom-player__btn_active");
-     }
+  for (var i = 0; i < this.controlButtons.length; i++) {
+    if (this.controlButtons[i].classList !== undefined)
+      this.controlButtons[i].classList.remove(this.mountOptions.controlBtnActive);
+  }
 }
 
 Player.prototype.updateTime = function(output, time) {
-     var minutes = Math.floor(time / 60);
-     var seconds = Math.ceil(time - minutes * 60);
-     var minutesOutput = (minutes < 10) ? (0 + minutes.toString()) : minutes.toString();
-     var secondsOutput = (seconds < 10) ? (0 + seconds.toString()) : seconds.toString();
-     output.innerHTML = minutesOutput + ":" + secondsOutput;
+  var minutes = Math.floor(time / 60);
+  var seconds = Math.ceil(time - minutes * 60);
+  var minutesOutput = (minutes < 10) ? (0 + minutes.toString()) : minutes.toString();
+  var secondsOutput = (seconds < 10) ? (0 + seconds.toString()) : seconds.toString();
+  output.innerHTML = minutesOutput + ":" + secondsOutput;
 }
 
 Player.prototype.initControls = function() {
-     this.player_progress.addEventListener("click", this.mouseMove.bind(this));
-     for (var i = 0; i < this.controlButtons.length; i++) {
-          this.controlButtons[i].addEventListener("click", this.togglePlay.bind(this));
-     }
+  this.volume_pick.active = false;
+  var volume_pick = this.volume_pick;
+
+  document.addEventListener("mouseup", releasePick);
+  this.volume_bar.addEventListener("mousedown", this.onVolumeClick.bind(this));
+  this.volume_bar.addEventListener("mousemove", this.onVolumeMove.bind(this));
+
+  this.progress_bar.addEventListener("click", this.onProgressBarClick.bind(this));
+  for (var i = 0; i < this.controlButtons.length; i++) {
+    this.controlButtons[i].addEventListener("click", this.togglePlay.bind(this));
+  }
+
+  function releasePick() {
+    volume_pick.active = false;
+  }
+
+}
+
+Player.prototype.onProgressBarClick = function(event) {
+  var widthTotal = getComputedStyle(this.progress_bar).width.slice(0, -2);
+  var rect = this.progress_bar.getBoundingClientRect();
+  var x = event.clientX - rect.left;
+  this.selectedProgress = Math.floor((x / widthTotal) * 100);
+  if (this.selectedProgress > this.calculateBuffer()) {
+    this.selectedProgress = this.calculateBuffer();
+  }
+  this.source.currentTime = this.source.duration * (this.selectedProgress / 100);
+  this.updateTime(this.player_timeNow, this.source.currentTime);
+  this.updateProgress();
+}
+
+Player.prototype.onVolumeClick = function(event) {
+  this.volume_pick.active = true;
+  this.setVolumePickPositionX(this.getSelectedX(event.clientX, this.volume_bar));
+}
+
+Player.prototype.onVolumePick = function(event) {
+  this.volume_pick.active = true;
+}
+Player.prototype.setVolumePickPositionX = function(x) {
+  var pick_width = getComputedStyle(this.volume_pick).width.slice(0, -2);
+  this.volume_pick.style.left = Math.floor(x - pick_width / 2) + 'px';
+}
+
+Player.prototype.onVolumeMove = function(event) {
+  var x = this.getSelectedX(event.clientX, this.volume_bar);
+  if (this.volume_pick.active && x >= 0 && x <= this.calculateWidth(this.volume_bar)) {
+    this.setVolumePickPositionX(x);
+    this.setVolume(this.getSelectedPercent(event.clientX, this.volume_bar));
+  }
+}
+Player.prototype.calculateWidth = function(obj) {
+  return getComputedStyle(obj).width.slice(0, -2);
+}
+Player.prototype.getSelectedX = function(mouseX, obj) {
+  return x = mouseX - obj.getBoundingClientRect().left;
+}
+Player.prototype.getSelectedPercent = function(mouseX, obj) {
+  return (Math.floor((this.getSelectedX(mouseX, obj) / this.calculateWidth(obj)) * 100)) / 100;
+}
+
+Player.prototype.setVolume = function(value) {
+  value = (value < 0) ? 0 : ((value > 1) ? 1 : value);
+  this.source.volume = value;
 }
 
 Player.prototype.startPlay = function(btn) {
-     this.source.play();
-     btn.innerHTML = "pause"
-     var beforePlay = this.playBack;
-     this.playBack = !this.playBack;
-     if (beforePlay == true) {
-          this.playBack = false;
-          this.source.pause();
-          btn.innerHTML = "play"
-          this.clearActiveState();
-     }
+  switch (btn) {
+    case (this.playBtn):
+      this.source.play();
+      this.playBack = true;
+      if (btn.classList !== undefined) {
+        btn.classList.add(this.mountOptions.controlBtnActive);
+      }
+      break;
+    case (this.pauseBtn):
+      this.source.pause();
+      this.playBack = false;
+      if (btn.classList !== undefined) {
+        btn.classList.add(this.mountOptions.controlBtnActive);
+      }
+      break;
+  }
 }
 
 Player.prototype.stopPlay = function(btn) {
-     this.source.pause();
-     this.playBack = false;
-     this.source.currentTime = 0;
-     this.updateTime(this.player_timeNow, this.source.currentTime);
-     this.updateProgress();
+  this.clearActiveState();
+  this.source.pause();
+  this.playBack = false;
+  this.source.currentTime = 0;
+  this.updateTime(this.player_timeNow, this.source.currentTime);
+  this.updateProgress();
 }
 Player.prototype.togglePlay = function(event) {
-     this.clearActiveState();
-     if (event.target.classList !== undefined)
-          event.target.classList.add("custom-player__btn_active");
-
-     switch (event.target) {
-          case this.playBtn:
-               this.startPlay(event.target);
-               break;
-          case this.stopBtn:
-               this.stopPlay();
-               break;
-     }
-     this.update();
+  this.clearActiveState();
+  if (event.target.classList !== undefined)
+    event.target.classList.add(this.mountOptions.controlBtnActive);
+  switch (event.target) {
+    case (this.playBtn):
+    case (this.pauseBtn):
+      this.startPlay(event.target);
+      break;
+    case this.stopBtn:
+      this.stopPlay();
+      break;
+  }
+  this.update();
 }
